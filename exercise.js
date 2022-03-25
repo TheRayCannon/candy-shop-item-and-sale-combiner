91833311a85971141e48de66e192caf15199ab72
Shawn Cannon <scanno16@ford.com>
2022-03-25 09:50:11 -0600

    complete function base

--- exercise.js
+++ exercise.js
@@ -1,13 +1,27 @@
 function getItemById(items, id) {
-    return items["id"]
+    return items.find(item => item.id === id)
 }
 
+function buildTransactions(sales, items) {
+    return sales.map(sale => {
+        const itemID = sale.itemID
+        const item = getItemById(items, itemID)
 
+        return {
+            ...sale,
+            description: item.description,
+            price: item.price,
+        }
+    })
+}
 
+function getTransactionsByItemDescription(transactions, description) {
+    return transactions.filter(transaction => transaction.description === description)
+}
 
 module.exports = {
-    // Uncomment these as you write them
+    //Uncomment these as you write them
     getItemById,
-    // buildTransactions,
-    // getTransactionsByItemDescription
+    buildTransactions,
+    getTransactionsByItemDescription
 }
